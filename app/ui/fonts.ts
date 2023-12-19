24aad421cb99e823c5896b70958453bf29632632
vrachmd <vieri.ag@yahoo.com>
2023-12-19 17:07:44 +0700

    Add All

---
+++ app/ui/fonts.ts
@@ -0,0 +1,16 @@
+import { Inter, Lusitana, Poppins } from 'next/font/google';
+ 
+export const inter = Inter({ 
+    subsets: ['latin'],
+    display: 'swap', 
+});
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
+
+export const poppins = Poppins({
+    weight: ['300', '400', '600', '700'],
+    subsets: ['latin'],
+});
